299c12bac849dd4cbbc939efc0884d5d72c2f5bc
Prabhat <prabhat.pal1204@gmail.com>
2022-05-17 11:23:44 +0530

    hope

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
-app.use('/home', indexRoute);
+app.use('/', indexRoute);
 app.use('/users', usersRoute);
 app.use('/quiz', quizRoute);
 
